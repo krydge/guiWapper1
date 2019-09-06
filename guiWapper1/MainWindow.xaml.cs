94f0147e282db2143abb52508d063eab3717d5b2
Michael Vance <mexvance04@gmail.com>
2019-09-05 23:28:03 -0600

    Got a lot of bindings in and connected to the cmd

--- guiWapper1/MainWindow.xaml.cs
+++ guiWapper1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,75 @@ namespace guiWapper1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StatusReport_Click(object sender, RoutedEventArgs e)
+        {
+            //var message = "YEET";
+            Background = Brushes.AliceBlue;
+
+        }
+
+        private void LogReport_Click(object sender, RoutedEventArgs e)
+        {
+            var command = "git log -p c:/aqdev/ver15";
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            //ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
+
+            //procStartInfo.RedirectStandardOutput = true;
+            //procStartInfo.UseShellExecute = false;
+            //procStartInfo.CreateNoWindow = true;
+
+            // wrap IDisposable into using (in order to release hProcess) 
+            using (Process process = new Process())
+            {
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.Arguments = "git log c:/aqdev/ver15";
+                process.StartInfo.FileName = "cmd";
+                process.Start();
+
+                ////strCommand is path and file name of command to run
+                //pProcess.StartInfo.FileName = "cmd";
+
+                ////strCommandParameters are parameters to pass to program
+                //pProcess.StartInfo.Arguments = command;
+
+                //pProcess.StartInfo.UseShellExecute = false;
+
+                ////Set output of program to be written to process output stream
+                //pProcess.StartInfo.RedirectStandardOutput = true;
+                //pProcess.StartInfo.CreateNoWindow = true;
+                //Process.Start("git", @"log c:/aqdev/ver15");
+
+
+                //Start the process
+                //pProcess.Start();
+
+                //Get program output
+                //OutputBlock.Text += pProcess.StandardOutput.ReadLine();
+                OutputBlock.Text = process.StandardOutput.ReadLine();
+
+                //Wait for process to finish
+                //process.WaitForExit();
+            }
+        }
+
+        private void InitRepo_Click(object sender, RoutedEventArgs e)
+        {
+            Background = Brushes.Lavender;
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            Background = Brushes.Coral;
+        }
+
+        private void Commit_Click(object sender, RoutedEventArgs e)
+        {
+            Background = Brushes.Chartreuse;
+            OutputBlock.Text += "\nCommit LoggeD";
         }
     }
 }
