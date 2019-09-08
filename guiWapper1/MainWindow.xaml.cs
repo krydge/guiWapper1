a05739b5dc5f3d1a9291fc32f42414ccd07fea27
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2019-09-07 22:46:18 -0600

    added check dark mode

--- guiWapper1/MainWindow.xaml.cs
+++ guiWapper1/MainWindow.xaml.cs
@@ -27,118 +27,135 @@ namespace guiWapper1
             InitializeComponent();
         }
 
-        private void CreateColors(List<OutputLine> list)
+        private void darkModeActivated(object sender, RoutedEventArgs e)
         {
-            foreach (OutputLine line in list)
+            var isChecked = darkmode.IsChecked;
+            if ((bool)isChecked)
             {
-                Run run = new Run("\n" + line.LineText );
-                switch(line.Color)
-                {
-                    case colors.WHITE:
+                Background = Brushes.DarkSlateGray;
+                locationTitle.Foreground = Brushes.White;
 
-                        run.Foreground = Brushes.White;
-                        break;
-                    case colors.YELLOW:
+            }
+            else
+            {
+                Background = Brushes.AliceBlue;
 
-                        run.Foreground = Brushes.Yellow;
-                        break;
-                    case colors.GREEN:
-
-                        run.Foreground = Brushes.Green;
-                        break;
-                    case colors.RED:
-
-                        run.Foreground = Brushes.Red;
-                        break;
-
-                }
-                OutputBlock.Inlines.Add(run);
+                locationTitle.Foreground = Brushes.Black;
             }
         }
 
-        private void LogReport_Click(object sender, RoutedEventArgs e)
-        {
-            OutputBlock.Inlines.Clear();
-            var command = "cd o:; git log --max-count=1 -p";
-            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
-            //ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
+        //private void CreateColors(List<OutputLine> list)
+        //{
+        //    foreach (OutputLine line in list)
+        //    {
+        //        Run run = new Run("\n" + line.LineText );
+        //        switch(line.Color)
+        //        {
+        //            case colors.WHITE:
 
-            //procStartInfo.RedirectStandardOutput = true;
-            //procStartInfo.UseShellExecute = false;
-            //procStartInfo.CreateNoWindow = true;
+        //                run.Foreground = Brushes.White;
+        //                break;
+        //            case colors.YELLOW:
 
-            // wrap IDisposable into using (in order to release hProcess)
-            using (Process process = new Process())
-            {
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.Arguments = command;
-                process.StartInfo.FileName = "powershell.exe";
-                process.Start();
-                string text;
-                int wait = 0;
-                while (process.StandardOutput.Peek() > -1 && wait < 1000)
-                {
-                    text = process.StandardOutput.ReadLine();
-                    if (text.StartsWith("+++") ||
-                        text.StartsWith("---") ||
-                        text.StartsWith("diff") ||
-                        text.StartsWith("index"))
-                    {
-                        Run run = new Run("\n" + text);
-                        run.Foreground = Brushes.Yellow;
-                        OutputBlock.Inlines.Add(run);
-                    }
-                    else if (text.StartsWith("+"))
-                    {
-                        Run run = new Run("\n" + text);
-                        run.Foreground = Brushes.Green;
-                        OutputBlock.Inlines.Add(run);
-                    }
-                    else if (text.StartsWith("-"))
-                    {
-                        Run run = new Run("\n" + text);
-                        run.Foreground = Brushes.Red;
-                        OutputBlock.Inlines.Add(run);
-                    }
-                    else
-                    {
-                        Run run = new Run("\n" + text);
-                        run.Foreground = Brushes.White;
-                        OutputBlock.Inlines.Add(run);
-                    }
-                    wait++;
-                }
+        //                run.Foreground = Brushes.Yellow;
+        //                break;
+        //            case colors.GREEN:
 
-                //while (process.StandardError.Peek() > -1)
-                //{
-                //    OutputBlock.Foreground = Brushes.Red;
-                //    OutputBlock.Text += process.StandardOutput.ReadLine();
-                //}
-                //process.WaitForExit();
-                //Wait for process to finish
-                //}
+        //                run.Foreground = Brushes.Green;
+        //                break;
+        //            case colors.RED:
 
-                process.WaitForExit();
-            }
-        }
+        //                run.Foreground = Brushes.Red;
+        //                break;
 
-        private void InitRepo_Click(object sender, RoutedEventArgs e)
-        {
-            Background = Brushes.Lavender;
-        }
+        //        }
+        //        OutputBlock.Inlines.Add(run);
+        //    }
+        //}
 
-        private void AddItem_Click(object sender, RoutedEventArgs e)
-        {
-            Background = Brushes.Coral;
-        }
+        //private void LogReport_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OutputBlock.Inlines.Clear();
+        //    var command = "cd o:; git log --max-count=1 -p";
+        //    System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+        //    //ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command);
 
-        private void Commit_Click(object sender, RoutedEventArgs e)
-        {
-            Background = Brushes.Chartreuse;
-            OutputBlock.Text += "\nCommit LoggeD";
-        }
+        //    //procStartInfo.RedirectStandardOutput = true;
+        //    //procStartInfo.UseShellExecute = false;
+        //    //procStartInfo.CreateNoWindow = true;
+
+        //    // wrap IDisposable into using (in order to release hProcess)
+        //    using (Process process = new Process())
+        //    {
+        //        process.StartInfo.RedirectStandardOutput = true;
+        //        process.StartInfo.RedirectStandardError = true;
+        //        process.StartInfo.UseShellExecute = false;
+        //        process.StartInfo.CreateNoWindow = true;
+        //        process.StartInfo.Arguments = command;
+        //        process.StartInfo.FileName = "powershell.exe";
+        //        process.Start();
+        //        string text;
+        //        int wait = 0;
+        //        while (process.StandardOutput.Peek() > -1 && wait < 1000)
+        //        {
+        //            text = process.StandardOutput.ReadLine();
+        //            if (text.StartsWith("+++") ||
+        //                text.StartsWith("---") ||
+        //                text.StartsWith("diff") ||
+        //                text.StartsWith("index"))
+        //            {
+        //                Run run = new Run("\n" + text);
+        //                run.Foreground = Brushes.Yellow;
+        //                OutputBlock.Inlines.Add(run);
+        //            }
+        //            else if (text.StartsWith("+"))
+        //            {
+        //                Run run = new Run("\n" + text);
+        //                run.Foreground = Brushes.Green;
+        //                OutputBlock.Inlines.Add(run);
+        //            }
+        //            else if (text.StartsWith("-"))
+        //            {
+        //                Run run = new Run("\n" + text);
+        //                run.Foreground = Brushes.Red;
+        //                OutputBlock.Inlines.Add(run);
+        //            }
+        //            else
+        //            {
+        //                Run run = new Run("\n" + text);
+        //                run.Foreground = Brushes.White;
+        //                OutputBlock.Inlines.Add(run);
+        //            }
+        //            wait++;
+        //        }
+
+        //        //while (process.StandardError.Peek() > -1)
+        //        //{
+        //        //    OutputBlock.Foreground = Brushes.Red;
+        //        //    OutputBlock.Text += process.StandardOutput.ReadLine();
+        //        //}
+        //        //process.WaitForExit();
+        //        //Wait for process to finish
+        //        //}
+
+        //        process.WaitForExit();
+        //    }
+        //}
+
+        //private void InitRepo_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Background = Brushes.Lavender;
+        //}
+
+        //private void AddItem_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Background = Brushes.Coral;
+        //}
+
+        //private void Commit_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Background = Brushes.Chartreuse;
+        //    OutputBlock.Text += "\nCommit LoggeD";
+        //}
     }
 }
